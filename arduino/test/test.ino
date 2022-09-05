c8e7ebd2b4774bf13f684fbee0194a3312e434df
isamu-isozaki <isamu.website@gmail.com>
2022-09-04 22:24:07 -0400

    finally walking!

--- arduino/test/test.ino
+++ arduino/test/test.ino
@@ -2,7 +2,7 @@
 
 #include <Servo.h>
 
-Servo servos[6];
+Servo servos[12];
 void setup() {
   // put your setup code here, to run once:
   servos[0].attach(13);
@@ -11,6 +11,12 @@ void setup() {
   servos[3].attach(10);
   servos[4].attach(9);
   servos[5].attach(8);
+  servos[6].attach(7);
+  servos[7].attach(6);
+  servos[8].attach(5);
+  servos[9].attach(4);
+  servos[10].attach(3);
+  servos[11].attach(2);
 }
 
 void loop() {
@@ -21,5 +27,11 @@ void loop() {
   servos[3].write(90);
   servos[4].write(90);
   servos[5].write(90);
+  servos[6].write(90);
+  servos[7].write(90);
+  servos[8].write(90);
+  servos[9].write(90);
+  servos[10].write(90);
+  servos[11].write(90);
   delay(0.5);
 }
